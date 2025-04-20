dad005a274fb11a4563a3e7d767c5b5f5a29651f
yut-wq <println856@gmail.com>
2025-04-20 14:05:48 +0900

    refactor: update URL pattern for question detail view to use 'specifics' path

--- djangotutorial/polls/urls.py
+++ djangotutorial/polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /polls/specifics/5/
+    path("specifics/<int:question_id>/", views.detail, name="detail"),
     # ex: /polls/5/results/
     path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
