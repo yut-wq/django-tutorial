a9f30ec38be47d5156ad00ce414f24021b5e8045
yut-wq <println856@gmail.com>
2025-04-23 22:21:56 +0900

    feat: restructure Question admin interface to use fieldsets for better organization

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -3,7 +3,10 @@ from .models import Question
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
 
 
 admin.site.register(Question, QuestionAdmin)
